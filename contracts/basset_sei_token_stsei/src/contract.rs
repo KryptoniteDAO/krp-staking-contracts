fb5272f0c8a2c778ec4bd0c10e216f3635f1c076
smith <smithmonnnnnnn@gmail.com>
2023-10-20 14:47:48 +0800

    fixed issue 13

--- contracts/basset_sei_token_stsei/src/contract.rs
+++ contracts/basset_sei_token_stsei/src/contract.rs
@@ -20,7 +20,7 @@ use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdError, 
 use cw20_base::allowances::{execute_decrease_allowance, execute_increase_allowance};
 use cw20_base::contract::instantiate as cw20_init;
 use cw20_base::contract::query as cw20_query;
-use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw20_base::msg::{ExecuteMsg, InstantiateMarketingInfo, InstantiateMsg, QueryMsg};
 
 use crate::handler::*;
 use crate::msg::TokenInitMsg;
@@ -40,7 +40,13 @@ pub fn instantiate(
         &deps.api.addr_canonicalize(&msg.hub_contract)?,
     )?;
 
-    if let Some(_marketing) = msg.marketing.clone() {
+    if let Some(marketing_info) = msg.marketing.clone() {
+        if let Some(_market_address) = marketing_info.marketing.clone() {
+        } else {
+            return Err(ContractError::Std(StdError::generic_err(
+                "The address field in the instantiate marketing info can't be none.",
+            )));
+        }
     } else {
         return Err(ContractError::Std(StdError::generic_err(
             "Instantiate marketing info can't be none.",
